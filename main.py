3c302792c9ef89dc31e314f8dfd5e8b34ed513c2
Yucheng Cai <99876883+Supernova0320@users.noreply.github.com>
2023-09-08 15:50:50 +0800

    导入库

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ from torch.utils.data import DataLoader
 
 # 准备参数
 n_epochs = 3  # 循环训练集的次数
+batch_size_train = 64
+batch_size_test = 1000
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 10
+random_seed = 1
+torch.manual_seed(random_seed)
+
